8ce53dae45d5deb12cf7283289ef49ea3f004196
Suzanne Schuber <suzanneschuber@gmail.com>
2023-07-14 15:05:52 -0700

    update program.cs file.

--- PierresBakery/Program.cs
+++ PierresBakery/Program.cs
@@ -7,8 +7,18 @@ namespace PierresBakery
   {
     static void Main()
     {
-      Bread testBread = new Bread(quantity: 1);
-      Console.WriteLine(testBread.GetType());
+      Console.WriteLine("*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*");
+      Console.WriteLine("Welcome to Pierre's Bakery!");
+      Console.WriteLine("We have a special on loaves of bread today. Buy 2, get 1 free. A single loaf costs $5");
+      Console.WriteLine("How many loaves of bread would you like to order today?");
+      Console.WriteLine("Please enter a number:");
+      string BreadOrder = Console.ReadLine();
+      int breadQty = int.Parse(BreadOrder);
+
+      Console.WriteLine("Enter another number:");
+
+
+      Triangle tri = new Triangle(length1, length2, length3);
     }
   }
 }
